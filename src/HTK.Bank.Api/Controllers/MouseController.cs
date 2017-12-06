ed44124f202c19b4043bc8ba36ce8a18130ac636
Mark Toadstool <mark.toadstool@gmail.com>
2017-12-06 22:25:55 +0100

    calculate based factors

--- src/HTK.Bank.Api/Controllers/MouseController.cs
+++ src/HTK.Bank.Api/Controllers/MouseController.cs
@@ -32,6 +32,13 @@ namespace HTK.Bank.Api.Controllers
             var headers = Request.Headers;
             var userName = headers.GetValues("UserName").First();
 
+            for (int i = 0; i < movements.Count - 2; i++)
+            {
+                movements[i].Direction = Direction(movements[i], movements[i + 1]);
+                movements[i].AngleOfCurvature = AngleOfCurvature(movements[i], movements[i + 1], movements[i + 2]);
+                movements[i].CurvatureDistance =CurvatureDistance(movements[i], movements[i + 1], movements[i + 2]);
+            }
+
             using (var db = new LiteDatabase(DATABASE_NAME))
             {
                 var collection = db.GetCollection<Batch>();
@@ -41,15 +48,75 @@ namespace HTK.Bank.Api.Controllers
                 batch.Movements = movements.ToArray();
                 collection.Insert(batch);
             }
-
-
-            if (movements != null)
-            {
-                return movements.Count;
-            }
-            return -1;
+           
+            return movements.Count;
         }
 
+         
+        private double? Direction(Movement m1, Movement m2)
+        {
+            double x = m2.X - m1.X;
+            double y = m2.Y - m1.Y;
+            double c = Math.Sqrt(x*x+y*y);
+            if(c==0)
+            {
+                return null;
+            }
 
+            var direction= Math.Acos(x / c)*180/Math.PI;
+            if(y<0)
+            {
+                return Math.Round(360 - direction,1);
+            }
+            return Math.Round(direction,1);
+            
+        }
+
+        private double? AngleOfCurvature(Movement m1, Movement m2, Movement m3)
+        {
+            double x1 = m2.X - m1.X;
+            double y1 = m2.Y - m1.Y;
+            double c1 = Math.Sqrt(x1 * x1 + y1 * y1);
+
+            double x2 = m3.X - m2.X;
+            double y2 = m3.Y - m2.Y;
+            double c2 = Math.Sqrt(x2 * x2 + y2 * y2);
+
+
+            double x3 = m3.X - m1.X;
+            double y3 = m3.Y - m1.Y;
+            double c3 = Math.Sqrt(x3 * x3 + y3 * y3);
+            if (c1 * c2 == 0)
+            {
+                return null;
+            }
+            var angle = Math.Acos((c1 * c1 + c2 * c2 - c3 * c3) / (2 * c1 * c2)) * 180 / Math.PI;
+
+            return Math.Round(angle,1);
+
+        }
+
+        private double? CurvatureDistance(Movement m1, Movement m2, Movement m3)
+        {
+            double x1 = m2.X - m1.X;
+            double y1 = m2.Y - m1.Y;
+            double c1 = Math.Sqrt(x1 * x1 + y1 * y1);
+
+            double x2 = m3.X - m2.X;
+            double y2 = m3.Y - m2.Y;
+            double c2 = Math.Sqrt(x2 * x2 + y2 * y2);
+
+
+            double x3 = m3.X - m1.X;
+            double y3 = m3.Y - m1.Y;
+            double c3 = Math.Sqrt(x3 * x3 + y3 * y3);
+            if (c1 * c3 == 0)
+            {
+                return null;
+            }
+            var angle = Math.Acos((c1 * c1 + c3 * c3 - c2 * c2) / (2 * c1 * c3));
+
+            return Math.Round(c1*Math.Sin(angle)/c3,2);
+        }
     }
 }
--- src/HTK.Bank.Api/Models/Movement.cs
+++ src/HTK.Bank.Api/Models/Movement.cs
@@ -11,6 +11,10 @@ namespace HTK.Bank.Api.Models
         public int X { get; set;}
         public int Y { get; set; }
         public string Type { get; set; }
+        public double? Direction { get; set; }
+        public double? AngleOfCurvature { get; set; }
+        public double? CurvatureDistance { get; set; }
+
 
     }
 }
