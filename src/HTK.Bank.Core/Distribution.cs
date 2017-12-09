1f57ee154c1076be74eb038cd248fc64829c950f
Dawid Dlugosz <dawid.dlugosz@gmail.com>
2017-12-09 13:42:38 +0100

    Normal Distribution for AngleOfCurvature

--- src/HTK.Bank.Core/Distribution.cs
+++ src/HTK.Bank.Core/Distribution.cs
@@ -1,9 +1,23 @@
-﻿using Accord.Statistics;
+﻿using System;
+using System.Collections.Generic;
+using Accord.Statistics;
 using Accord.Statistics.Distributions.Univariate;
+using HTK.Bank.Core.Models;
+using System.Linq;
 
 namespace HTK.Bank.Core
 {
     public class Distribution
     {
+        public object CreateDistribution(List<IMovement> mousMovements)
+        {
+            var arrayOfAngleOfCurvature = mousMovements
+                                            .Select(_ => _.AngleOfCurvature.HasValue ? _.AngleOfCurvature.Value/1000 : 0)
+                                            .ToArray();
+            var normalDistribution = new NormalDistribution();
+            normalDistribution.Fit(arrayOfAngleOfCurvature);
+
+            return normalDistribution.ToString();
+        }
     }
 }
--- src/HTK.Bank.UnitTest/DistributionTest.cs
+++ src/HTK.Bank.UnitTest/DistributionTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HTK.Bank.Core;
+using System.Collections.Generic;
+using HTK.Bank.Core.Models;
 
 namespace HTK.Bank.UnitTest
 {
@@ -9,7 +12,28 @@ namespace HTK.Bank.UnitTest
         [TestMethod]
         public void SHOULD_Generate_Distribution_based_on_list_of_movements()
         {
+            var mousMovements = new List<IMovement>
+            {
+                CreateMockedMovement(150),
+                CreateMockedMovement(270),
+                CreateMockedMovement(10),
+                CreateMockedMovement(150),
+                CreateMockedMovement(280),
+                CreateMockedMovement(150),
+                CreateMockedMovement(359),
+            };
+            var business = new Distribution();
 
+            var distribution = business.CreateDistribution(mousMovements);
+            Console.WriteLine(distribution);
+        }
+
+        private IMovement CreateMockedMovement(double angleOfCurvature)
+        {
+            var mousMove = new Moq.Mock<IMovement>();
+            mousMove.Setup(_ => _.AngleOfCurvature).Returns(angleOfCurvature);
+
+            return mousMove.Object;
         }
     }
 }
